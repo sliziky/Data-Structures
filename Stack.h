4b8e7897c3dd7671ee48a329127e6418ecc8d376
Peter Bolfa <35201605+sliziky@users.noreply.github.com>
2018-06-21 10:40:33 +0200

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -12,46 +12,45 @@ public:
 	Stack& operator=(const Stack<T> &obj);
 	~Stack();
 	T pop();
-        void swap(Stack<T> &first, Stack<T> &second);
+    void swap(Stack<T> &first, Stack<T> &second);
 	void push(const int& num);
 	void resize(const int& size);
 	void print();
+
 private:
 	T * buffer;
 	unsigned int actualSize;
 	unsigned int maxSize;	
 };
 template <class T>
-Stack<T>::Stack() : maxSize(DEFAULT_SIZE), actualSize(0), buffer(new T[DEFAULT_SIZE]) {};
+Stack<T>::Stack() :  // default constructor
+    maxSize(DEFAULT_SIZE), 
+    actualSize(0), 
+    buffer(new T[DEFAULT_SIZE]) {}
 
 template <class T>
-Stack<T>::Stack(const Stack<T> &obj) {
-	buffer = new T[obj.actualSize];
-	this->actualSize = obj.actualSize;
-	this->maxSize = obj.maxSize;
+Stack<T>::Stack(const Stack<T> &obj):  // copy constructor 
+    buffer(new T[obj.actualSize]),
+    actualSize(obj.actualSize),
+    maxSize(obj.maxSize) {
 	std::copy(obj.buffer, obj.buffer + obj.actualSize, buffer);
 }      
 
 template <class T>
-Stack<T>&  Stack<T>::operator=(const Stack<T> &obj) {
+Stack<T>&  Stack<T>::operator=(const Stack<T> &obj) {  // copy assignment
     swap(*this, obj);
 	return *this;
 }
 
 template <class T>
-void Stack<T>::swap(Stack<T> &first, Stack<T> &second) {
+void Stack<T>::swap(Stack<T> &first, Stack<T> &second) {  // copynswap idiom 
     std::swap(first.buffer,second.buffer);
     std::swap(first.actualSize, second.actualSize);
     std::swap(first.maxSize, second.maxSize);
 }
 
 template <class T>
-Stack<T>::~Stack() {
-	delete[] buffer;
-}
-
-template <class T>
-void Stack<T>::push(const int& num) {
+void Stack<T>::push(const int& num) {  // push number into stack
 	if (actualSize >= maxSize) {
 		maxSize += 5;
 		resize(maxSize);
@@ -60,7 +59,7 @@ void Stack<T>::push(const int& num) {
 }
 
 template <class T>
-void Stack<T>::resize(const int& size) {
+void Stack<T>::resize(const int& size) {  // resize stack and copy elements into new stack
 	T* temp = new T[size];
 	std::copy(buffer, buffer + actualSize, temp);
 	delete[] buffer;
@@ -68,16 +67,26 @@ void Stack<T>::resize(const int& size) {
 }
 
 template <class T>
-void Stack<T>::print() {
+void Stack<T>::print() {  
+    /*for (auto i : buffer) {
+        std::cout << i << std::endl;
+    }*/
+
 	for (int i = 0; i < actualSize; i++) {
 		std::cout << buffer[i] << std::endl;
 	}
 }
 
+
 template <class T>
 T Stack<T>::pop() {
-	if (actualSize > 0)
+	if (actualSize > 0)  
 		return buffer[--actualSize];
+}
+
+template <class T>
+Stack<T>::~Stack() {  // destructor
+	delete[] buffer;
 }
 
 #endif
